d0c4d135d5c2700f2c15c12b16c6ee7210aef105
Sujal Mandal <ss6sujal@gmail.com>
2022-03-09 23:01:05 +0400

    linked list

---
+++ src/algorithms/kotlin/LinkedList.kt
@@ -0,0 +1,40 @@
+package algorithms.kotlin
+
+data class Node(val value:Int, var next : Node? = null)
+
+class LinkedList(var root: Node? = null){
+    var currentNode : Node? = null;
+}
+
+fun LinkedList.add(node : Node) {
+    if(root==null) {
+        this.root = node;
+        this.currentNode = root;
+    }
+    else {
+        this.currentNode?.next = node;
+        this.currentNode = node;
+    }
+}
+
+fun LinkedList.print(){
+    val printer = fun (node : Node?) {
+        var temp = node;
+        while(temp!=null){
+            println(temp.value);
+            temp = temp?.next;
+        }
+    }
+    printer(this.root);
+}
+
+fun main() {
+    val ll = LinkedList()
+    with(ll){
+        add(Node(1));
+        add(Node(2));
+        add(Node(3));
+        add(Node(4));
+    }
+    ll.print();
+}

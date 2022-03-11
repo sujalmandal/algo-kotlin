775e724866a0a7e538520839bcba540a4bc3dcb9
Sujal Mandal <ss6sujal@gmail.com>
2022-03-11 18:27:01 +0400

    encapsulate

--- src/algorithms/kotlin/GraphNodeCounter.kt
+++ src/algorithms/kotlin/GraphNodeCounter.kt
@@ -1,27 +1,57 @@
 package algorithms.kotlin
 
-val adjacencyMatrix : Array<Array<Int>> = arrayOf(
-    arrayOf(0,0,0,0),
-    arrayOf(0,0,0,0),
-    arrayOf(0,0,0,0),
-    arrayOf(0,0,0,0)
-);
+class Graph(private val size:Int){
+    /* static variables */
+    companion object {
+        private const val DELIM = "->";
+    }
 
-fun Array<Array<Int>>.print(){
-    for(row: Array<Int> in adjacencyMatrix) {
-        for(el: Int in row){
-            print("${if(el==1) "o" else "x"} ");
+    private var adjacencyMatrix : MutableList<MutableList<Int>> = mutableListOf();
+
+    init {
+        for (i in 0 until size){
+            var row: MutableList<Int> = mutableListOf();
+            for(j in 0 until size){
+                row.add(0);
+            }
+            adjacencyMatrix.add(row);
         }
+    }
+
+    fun fill(connections:Array<String>){
+        for(connection :String in connections){
+            val nodes : List<String> = connection.split(DELIM);
+            val fromNode: Int = Integer.parseInt(nodes[0]);
+            val toNode: Int = Integer.parseInt(nodes[1]);
+            adjacencyMatrix[fromNode-1][toNode-1] = 1;
+        }
+    }
+
+    fun print(){
+        print("    ");
+        for(i in 0 until this.size) print("${i+1} ")
         println();
+        var i = 1;
+        for(row: List<Int> in this.adjacencyMatrix) {
+            print("${i++} | ")
+            for(el: Int in row){
+                print("${if(el==1) "o" else "x"} ");
+            }
+            println();
+        }
     }
 }
 
-/*
+fun main(){
+    val myGraph : Graph = Graph(5);
+    /*
         1----3
         |
         2----4
  */
-val connections:Array<String> = arrayOf("1->3","1->2","2->4")
+    /* fill the edges */
+    myGraph.fill(arrayOf("1->3","1->2","2->4"));
+    myGraph.print();
 /*
     we need to construct an adjacency matrix that looks like the below matrix
          1  2  3  4
@@ -32,15 +62,5 @@ val connections:Array<String> = arrayOf("1->3","1->2","2->4")
     4  | x  x  x  x
  */
 
-private const val DELIM = "->"
-
-fun main(){
-    for(connection :String in connections){
-        val nodes : List<String> = connection.split(DELIM);
-        val fromNode: Int = Integer.parseInt(nodes[0]);
-        val toNode: Int = Integer.parseInt(nodes[1]);
-        adjacencyMatrix[fromNode-1][toNode-1] = 1;
-    }
-    adjacencyMatrix.print();
 }
 
